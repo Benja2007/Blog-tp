fecfdd0bc76c884f943677b23a857f297c21c0cf
Benja2007 <130391949+Benja2007@users.noreply.github.com>
2023-10-17 14:41:05 -0300

    poder entrar a los post

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-// App.js
-import React, { useState } from 'react';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import PostList from './components/PostList';
-import AddPostForm from './components/AddPostForm';
+import PostDetail from './components/PostDetail'; // Componente para mostrar el contenido completo
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'Publicaciones:', content: '' },
-  ]);
-
-  const addPost = (newPost) => {
-    // Agrega una nueva publicación al estado de posts
-    setPosts([...posts, newPost]);
-  };
+  // ...
 
   return (
     <div className="App">
       <Header />
       <main>
-        <h1>Añadi tu Post</h1>
-        <AddPostForm onAddPost={addPost} />
-        <PostList posts={posts} />
+        <Router>
+          <Route path="/" exact render={() => <PostList posts={posts} />} />
+          <Route path="/post/:id" component={PostDetail} />
+        </Router>
       </main>
     </div>
   );
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,15 +1,24 @@
-// PostList.js
-import React from 'react';
-import Post from './Post'; // Asegúrate de importar el componente Post
+import React, { useState } from 'react';
 
 function PostList({ posts }) {
+  const [expandedPosts, setExpandedPosts] = useState([]);
+
+  const toggleExpand = (postId) => {
+    if (expandedPosts.includes(postId)) {
+      setExpandedPosts(expandedPosts.filter((id) => id !== postId));
+    } else {
+      setExpandedPosts([...expandedPosts, postId]);
+    }
+  };
+
   return (
     <div className="post-list">
       <h2>Lista de Publicaciones</h2>
       <ul>
         {posts.map((post) => (
           <li key={post.id}>
-            <Post title={post.title} content={post.content} />
+            <h3 onClick={() => toggleExpand(post.id)}>{post.title}</h3>
+            {expandedPosts.includes(post.id) && <p>{post.content}</p>}
           </li>
         ))}
       </ul>
