2a6f931c8a5383e6408c158d2134a6528c1fd9b8
Benja2007 <130391949+Benja2007@users.noreply.github.com>
2023-10-19 21:59:48 -0300

    ?

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className='App-header'>
     <header>
-      <h1>HOME</h1>
+      <h1>Blog</h1>
       {/* Agrega aquí la barra de navegación u otros elementos del encabezado */}
     </header>
     </div>
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,24 +1,15 @@
-import React, { useState } from 'react';
+// PostList.js
+import React from 'react';
+import Post from './Post'; // Asegúrate de importar el componente Post
 
 function PostList({ posts }) {
-  const [expandedPosts, setExpandedPosts] = useState([]);
-
-  const toggleExpand = (postId) => {
-    if (expandedPosts.includes(postId)) {
-      setExpandedPosts(expandedPosts.filter((id) => id !== postId));
-    } else {
-      setExpandedPosts([...expandedPosts, postId]);
-    }
-  };
-
   return (
     <div className="post-list">
       <h2>Lista de Publicaciones</h2>
       <ul>
         {posts.map((post) => (
           <li key={post.id}>
-            <h3 onClick={() => toggleExpand(post.id)}>{post.title}</h3>
-            {expandedPosts.includes(post.id) && <p>{post.content}</p>}
+            <Post title={post.title} content={post.content} />
           </li>
         ))}
       </ul>
