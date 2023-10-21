d46edb19fba16833301276962715853cb67bbc79
Benja2007 <130391949+Benja2007@users.noreply.github.com>
2023-10-20 23:00:32 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'; // Importa BrowserRouter, Link y Route
 import Header from './components/Header';
-import Post from './components/Post';
-import AddPostForm from './components/AddPostForm';
 import PostList from './components/PostList';
+import AddPostForm from './components/AddPostForm';
 
 function App() {
+  // Definimos un estado para las publicaciones
+  const [posts, setPosts] = useState([]);
+
+  // Función para agregar una nueva publicación al estado
+  const addPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
+
   return (
     <Router>
       <div className="App">
         <Header />
         <main>
           <Route path="/" exact component={PostList} />
-          <Route path="/add" component={AddPostForm} />
-          <Route path="/post/:id" component={Post} />
+          <Route path="/add" render={() => <AddPostForm onAddPost={addPost} />} />
         </main>
       </div>
     </Router>
