035c60d9fd43ff57aa710769e28967dd416cb674
Benja2007 <130391949+Benja2007@users.noreply.github.com>
2023-10-20 23:32:33 -0300

    deploy

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import PostForm from './PostForm';
 import PostList from './PostList';
 
@@ -7,10 +7,8 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
           <Route path="/post/:id" component={PostList} />
           <Route path="/" exact component={PostForm} />
-        </Switch>
       </div>
     </Router>
   );
